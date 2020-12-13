02ffac7ae90aea18598607e524e6d179058427b7
Pedro Victor de Brito Cordeiro <pedrocga1@gmail.com>
2020-12-12 16:44:48 -0800

    Better optimized Journey generator.

--- Journeys/ChallengeBuilder.cs
+++ Journeys/ChallengeBuilder.cs
@@ -20,31 +20,81 @@ namespace Journeys
         public Journey GenerateRandomJourney(int x, int y, char direction)
         {
             (int X, int Y, char Direction) Initial = (x, y, direction);
-            int numberOfCommands = _random.Next(15);
-            StringBuilder commandsBuilder = new StringBuilder();
             (int X, int Y, char Direction) Final = Initial;
-
+            int numberOfCommands = _random.Next(15);
+            StringBuilder allCommands = new StringBuilder();
+            
             for (int i = 0; i < numberOfCommands; i++)
             {
-                switch (_random.Next(3))
+                allCommands.Append(_validCommands[_random.Next(_validCommands.Length)]);
+            }
+            string effectiveCommands = Simplify(allCommands.ToString());
+
+            for (int i = 0; i < effectiveCommands.Length; i++)
+            {
+                switch (effectiveCommands[i])
                 {
-                    case 0:
-                        TurnRight(commandsBuilder, ref Final);
+                    case 'R':
+                        TurnRight(ref Final);
                         break;
-                    case 1:
-                        TurnLeft(commandsBuilder, ref Final);
+                    case 'L':
+                        TurnLeft(ref Final);
                         break;
-                    case 2:
-                        GoFoward(commandsBuilder, ref Final);
+                    case 'F':
+                        GoFoward(ref Final);
                         break;
                 }
             }
-            return new Journey { Initial = Initial, Commands = commandsBuilder.ToString(), Final = Final };
+            return new Journey { Initial = Initial, Commands = allCommands.ToString(), Final = Final };
         }
 
-        private void GoFoward(StringBuilder commandsBuilder, ref (int X, int Y, char Direction) Final)
+        private string Simplify(string commands)
         {
-            commandsBuilder.Append('F');
+            StringBuilder effectiveCommands = new StringBuilder();
+            int turnsRight = 0;
+            foreach (char command in commands)
+            {
+                switch (command)
+                {
+                    case 'R':
+                        turnsRight++;
+                        break;
+                    case 'L':
+                        turnsRight--;
+                        break;
+                    case 'F':
+                        turnsRight = ApplyTurns(effectiveCommands, turnsRight);
+                        effectiveCommands.Append('F');
+                        turnsRight = 0;
+                        break;
+                }
+            }
+            turnsRight = ApplyTurns(effectiveCommands, turnsRight);
+            return effectiveCommands.ToString();
+        }
+
+        private static int ApplyTurns(StringBuilder effectiveCommands, int turnsRight)
+        {
+            if (turnsRight > 0)
+            {
+                while (turnsRight-- > 0)
+                {
+                    effectiveCommands.Append('R');
+                }
+            }
+            else if (turnsRight < 0)
+            {
+                while (turnsRight++ < 0)
+                {
+                    effectiveCommands.Append('L');
+                }
+            }
+
+            return turnsRight;
+        }
+
+        private void GoFoward(ref (int X, int Y, char Direction) Final)
+        {
             switch (Final.Direction)
             {
                 case 'N':
@@ -62,9 +112,8 @@ namespace Journeys
             }
         }
 
-        private void TurnLeft(StringBuilder commandsBuilder, ref (int X, int Y, char Direction) Final)
+        private void TurnLeft(ref (int X, int Y, char Direction) Final)
         {
-            commandsBuilder.Append('L');
             for (int j = _validDirections.Length - 1; j > 0; j--)
             {
                 if (_validDirections[j] == Final.Direction)
@@ -75,9 +124,8 @@ namespace Journeys
             }
         }
 
-        private void TurnRight(StringBuilder commandsBuilder, ref (int X, int Y, char Direction) Final)
+        private void TurnRight(ref (int X, int Y, char Direction) Final)
         {
-            commandsBuilder.Append('R');
             for (int j = 0; j < _validDirections.Length - 1; j++)
             {
                 if (_validDirections[j] == Final.Direction)
